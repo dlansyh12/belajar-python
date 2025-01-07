cae0ccfb1142fe565fe331b15bb162f9ec404757
Muhammad Fadlan Ardhiansyah <168330033+dlansyh12@users.noreply.github.com>
2025-01-07 09:43:04 +0700

    Add files via upload

---
+++ uas.py
@@ -0,0 +1,28 @@
+# SOAL MATERI 4.1
+# def cek_angka (a,b,c):
+#     #parameter beda  apa engga
+#     if a != b and b != c and a != c:
+#         if a + b == c or a + c == b or b + c == a:
+#             return True
+#     return False
+
+# a = int(input("Masukkan angka a: "))
+# b = int(input("Masukkan angka b: "))
+# c = int(input("Masukkan angka c: "))
+# print(cek_angka(a,b,c))
+
+# SOAL MATERI  4.2 
+# def cek_belakang (a,b,c):
+#     # Cek parameter
+#     digit_pertama = a % 10
+#     digit_kedua = b % 10
+#     digit_ketiga = c % 10
+
+#     if digit_pertama == digit_kedua or digit_pertama == digit_ketiga or digit_kedua == digit_ketiga:
+#         return True
+#     return False
+
+# a = int(input("Masukkan angka a: "))
+# b = int(input("Masukkan angka b: "))
+# c = int(input("Masukkan angka c: "))
+# print(cek_belakang(a,b,c))
